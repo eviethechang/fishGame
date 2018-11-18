d9294a6c293155e778396c41ca19c21c988dc5d6
Evie Chang <43763929+eviethechang@users.noreply.github.com>
2018-11-17 17:18:16 -0800

    Player sprite implemented, random spawning of food

---
+++ fishGame/fishGame/phaser/fishGame/js/main.js
@@ -0,0 +1,84 @@
+var game = new Phaser.Game(1000, 700, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var total = 0;
+var timer = 0;
+
+function preload() {
+
+    game.load.image('ball', 'assets/img/blueOrb.png');
+	game.load.spritesheet('koi', 'assets/img/KoiSwim.png', 1000, 400);
+
+}
+
+var sprite;
+
+function create() {
+
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+    foods = game.add.group();
+	foods.enableBody = true;
+	
+	sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'koi');
+	game.physics.arcade.enable(sprite);
+	sprite.enableBody = true;
+	//sprite.body.setSize(60, 50, 30, 18);
+	sprite.anchor.setTo(0.5, 0.5);
+	sprite.angle = 90;
+	sprite.scale.setTo(0.2, 0.2);
+	sprite.animations.add('swim', [0,1], 5, true);
+
+	
+
+	
+}
+
+function spawnFood(){
+	
+	//var food = game.add.sprite(game.world.randomX, game.world.randomY, 'ball');
+	var food = foods.create(game.world.randomX, game.world.randomY, 'ball');
+	//food.enableBody = true;
+	game.physics.arcade.enable(food);
+	total++;
+	timer = game.time.now + 1000;
+	
+}
+
+function update() {
+	
+	sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+    //  only move when you click
+    if (game.input.mousePointer.isDown)
+    {
+        //  400 is the speed it will move towards the mouse
+        game.physics.arcade.moveToPointer(sprite, 300);
+		sprite.animations.play('swim')
+
+        //  if it's overlapping the mouse, don't move any more
+        if (Phaser.Rectangle.contains(sprite.body, game.input.x, game.input.y))
+        {
+            sprite.body.velocity.setTo(0, 0);
+        }
+    }
+    else
+    {
+        sprite.body.velocity.setTo(0, 0);
+    }
+	
+	if (total < 5 && game.time.now > timer)
+    {
+        spawnFood();
+    }
+	
+	game.physics.arcade.overlap(sprite, foods, this.collectFood, null, this);
+
+}
+
+
+
+function collectFood(sprite, food) {
+		// Removes the food from the screen
+		food.kill();
+		console.log("cheesecake");
+		//plink.play();
+		//score += 1; //score updating
+		//scoreText.text = 'Score: ' + score;
+}
